7ab9a1584e5c21cc2a6e9b2875e4b91528af5425
Egan Done <edone@sympatico.ca>
2019-07-27 19:16:35 -0400

    submission Bite 125 @ codechalleng.es

--- 125/tribe.py
+++ 125/tribe.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 from bs4 import BeautifulSoup
 import requests
+import re
 
 AMAZON = "amazon.com"
 TIM_BLOG = 'https://bit.ly/2NBnZ6P'
@@ -19,4 +20,14 @@ def get_top_books(content=None, limit=5):
        and count them, return the top "limit" books (default 5)"""
     if content is None:
         content = load_page()
-    # code here ...
+    # Just prase the content as HTML
+    soup = BeautifulSoup(content, 'html.parser')
+    # Look for all the links that include amazon in the href
+    tags = soup.find_all('a', href=re.compile('amazon'))
+    # The content of each link will be the title.  Just use
+    # a counter to count the occurance of each title
+    title_counter = Counter([next(tag.strings) for tag in tags])
+
+    # Return the first element of each counter entry (the title)
+    # ... the second element will be the count but is not needed here.
+    return [title[0] for title in title_counter.most_common(limit)]
