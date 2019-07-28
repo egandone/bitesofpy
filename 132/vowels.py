fe66ae80d3ed301106a72557f33589b8baad832a
Egan Done <edone@sympatico.ca>
2019-07-27 20:06:26 -0400

    submission Bite 132 @ codechalleng.es

--- 132/vowels.py
+++ 132/vowels.py
@@ -1,8 +1,12 @@
-VOWELS = list('aeiou')
-
+import re
+VOWELS = 'aeiou'
 
 def get_word_max_vowels(text):
-    """Get the case insensitive word in text that has most vowels.
+   """Get the case insensitive word in text that has most vowels.
        Return a tuple of the matching word and the vowel count, e.g.
        ('object-oriented', 6)"""
-    pass
+   
+   vowel_locator = re.compile(f'[{VOWELS}]')
+   vowel_counts = [(w, len(vowel_locator.findall(w))) for w in text.split()]
+   vowel_counts = sorted(vowel_counts, key=lambda t: t[1], reverse=True)
+   return vowel_counts[0]
