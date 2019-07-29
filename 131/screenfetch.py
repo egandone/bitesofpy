86943d324376f615bc504fbc25e7bd2b8704d440
Egan Done <edone@sympatico.ca>
2019-07-29 08:29:33 -0400

    submission Bite 131 @ codechalleng.es

---
+++ 131/screenfetch.py
@@ -0,0 +1,38 @@
+import re
+
+output = """
+                                       mohh@SERENiTY
+ MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        OS: Mint 19 tara
+ MMm----::-://////////////oymNMd+'     Kernel: x86_64 Linux 4.15.0-34-generic
+ MMd      /++                -sNMd:    Uptime: 1d 4m
+ MMNso/'  dMM    '.::-. .-::.' .hMN:   Packages: 2351
+ ddddMMh  dMM   :hNMNMNhNMNMNh: 'NMm   Shell: zsh 5.4.2
+     NMm  dMM  .NMN/-+MMM+-/NMN' dMM   Resolution: 1366x768
+     NMm  dMM  -MMm  'MMM   dMM. dMM   DE: Cinnamon 3.8.9
+     NMm  dMM  -MMm  'MMM   dMM. dMM   WM: Muffin
+     NMm  dMM  .mmd  'mmm   yMM. dMM   WM Theme: Linux Mint (Mint-Y)
+     NMm  dMM'  ..'   ...   ydm. dMM   GTK Theme: Mint-Y [GTK2/3]
+     hMM- +MMd/-------...-:sdds  dMM   Icon Theme: Mint-Y
+     -NMm- :hNMNNNmdddddddddy/'  dMM   Font: Noto Sans 9
+      -dMNs-''-::::-------.''    dMM   CPU: AMD A10-7400P Radeon R6, 10 Compute Cores 4C+6G @ 4x 2.5GHz [101.0°C]
+       '/dMNmy+/:-------------:/yMMM   GPU: AMD KAVERI (DRM 2.50.0 / 4.15.0-34-generic, LLVM 6.0.0)
+          ./ydNMMMMMMMMMMMMMMMMMMMMM   RAM: 1886MiB / 6915MiB
+             \.MMMMMMMMMMMMMMMMMMM    
+"""
+
+
+def sysinfo_scrape(output):
+   """Scrapes the output from screenfetch and returns a dictionary"""
+   attributes = {}
+   # Just interested in non-blank lines
+   lines = [line for line in output.split('\n') if line]
+   # Assume the first line gives the system name
+   m = re.match('^.*\s\s([^\s]*)$', lines[0])
+   attributes['Name'] = m.group(1).strip()
+   # For all other lines we match "name: value" at the end of the line
+   for line in lines[1:]:
+      m = re.match('^.*\s\s([^:]*):(.*)$', line)
+      if m:
+         attributes[m.group(1)] = m.group(2).strip()
+   return attributes
+   
