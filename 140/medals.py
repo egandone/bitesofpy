b08dbc5468ba4a3b8cd739eb6343c7f6e53906ce
Egan Done <edone@sympatico.ca>
2019-08-24 09:51:30 -0400

    submission Bite 140 @ codechalleng.es

---
+++ 140/medals.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from collections import Counter
+
+data = "http://projects.bobbelderbos.com/data/summer.csv"
+
+def athletes_most_medals():
+    summer = pd.read_csv(data)
+    name_counter = Counter(summer.Athlete)
+    return {t[0]:t[1] for t in name_counter.most_common()}
