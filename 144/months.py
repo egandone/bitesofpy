be8df1200ffa55a89d138f469f6110903d887b14
Egan Done <edone@sympatico.ca>
2019-09-02 10:55:55 -0400

    submission Bite 144 @ codechalleng.es

--- 144/months.py
+++ 144/months.py
@@ -8,25 +8,32 @@ MONTHS_PER_YEAR = 12
 
 
 def calc_months_passed(year, month, day):
-    """Construct a date object from the passed in arguments.
-       If this fails due to bad inputs reraise the exception.
-       Also if the new date is < START_DATE raise a ValueError.
+   """Construct a date object from the passed in arguments.
+      If this fails due to bad inputs reraise the exception.
+      Also if the new date is < START_DATE raise a ValueError.
 
-       Then calculate how many months have passed since the
-       START_DATE constant. We suggest using dateutil.relativedelta!
+      Then calculate how many months have passed since the
+      START_DATE constant. We suggest using dateutil.relativedelta!
 
-       One rule: if a new month is >= 10 (MIN_DAYS_TO_COUNT_AS_MONTH)
-       days in, it counts as an extra  month.
+      One rule: if a new month is >= 10 (MIN_DAYS_TO_COUNT_AS_MONTH)
+      days in, it counts as an extra  month.
 
-       For example:
-       date(2018, 11, 10) = 9 days in => 0 months
-       date(2018, 11, 11) = 10 days in => 1 month
-       date(2018, 12, 11) = 1 month + 10 days in => 2 months
-       date(2019, 12, 11) = 1 year + 1 month + 10 days in => 14 months
-       etc.
+      For example:
+      date(2018, 11, 10) = 9 days in => 0 months
+      date(2018, 11, 11) = 10 days in => 1 month
+      date(2018, 12, 11) = 1 month + 10 days in => 2 months
+      date(2019, 12, 11) = 1 year + 1 month + 10 days in => 14 months
+      etc.
 
-       See the tests for more examples.
+      See the tests for more examples.
 
-       Return the number of months passed int.
-    """
-    pass
+      Return the number of months passed int.
+   """
+   d = date(year, month, day)
+   if d < START_DATE:
+      raise(ValueError(f"date must be before {START_DATE:%Y-%m-%d}"))
+   rd = relativedelta(d, START_DATE)
+   months = (rd.years * MONTHS_PER_YEAR) + rd.months
+   if rd.days >= MIN_DAYS_TO_COUNT_AS_MONTH:
+      months += 1
+   return months
