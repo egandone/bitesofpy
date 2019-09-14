9c86bcf0a33b3c106cf157cbd2f20b663301ec31
Egan Done <edone@sympatico.ca>
2019-09-14 14:28:36 -0400

    submission Bite 179 @ codechalleng.es

--- 179/comments.py
+++ 179/comments.py
@@ -1,3 +1,20 @@
+import re
+
 def strip_comments(code):
-    # see Bite description
-    pass
+    # Find all block comments.  Make sure . matches newline
+    # and use non-agressive capture to sure we don't grab
+    # across multiple independent blocks
+    block_comments = re.compile(r'\s*""".*?"""', re.DOTALL)
+    code = block_comments.sub('', code)
+
+    #
+    # Strip all one line comments.
+    one_line_comments = re.compile(r'\n\s*#.*')
+    code = one_line_comments.sub('', code)
+
+    # 
+    # Strip any trailing comments that are not embedded
+    # within a string
+    trailing_comments = re.compile(r'#[^\'"]*$')
+    code = trailing_comments.sub('', code)
+    return code
