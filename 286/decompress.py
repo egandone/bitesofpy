1a4375aeb4dc361e6755a154bfc2376912770dea
egandone <edone@sympatico.ca>
2020-10-05 09:13:10 -0400

    Working, but not pretty

--- 286/decompress.py
+++ 286/decompress.py
@@ -2,4 +2,14 @@ from typing import Dict
 
 
 def decompress(string: str, table: Dict[str, str]) -> str:
-    ...
+    result = string
+    while result and (set(table.keys()).intersection(set(result))):
+        new_result = ''
+        for c in result:
+            if c in table:
+                new_result += table[c]
+            else:
+                new_result += c
+        result = new_result
+
+    return result
