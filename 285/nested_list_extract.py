07141a74a7c8de95190b4ea136375d5f27c03f51
Egan Done <edone@sympatico.ca>
2020-10-04 22:08:52 -0400

    submission Bite 285 @ codechalleng.es

--- 285/nested_list_extract.py
+++ 285/nested_list_extract.py
@@ -1,5 +1,32 @@
+import re
+
+
+def flatten_list(data):
+    flat_list = []
+    if isinstance(data, list):
+        for d in data:
+            flat_list.extend(flatten_list(d))
+    else:
+        flat_list.append(data)
+    return flat_list
+
+
 def extract_ipv4(data):
     """
     Given a nested list of data return a list of IPv4 address information that can be extracted
     """
-    return
+    results = []
+    flattened_data = flatten_list(data)
+    addr, mask = None, None
+    for i, v in enumerate(flattened_data):
+        if v == 'ip' and (i < len(flattened_data) - 1):
+            addr = flattened_data[i+1]
+            if addr:
+                addr = addr.strip('"')
+        elif v == 'mask' and (i < len(flattened_data) - 1):
+            mask = flattened_data[i+1]
+        if addr and mask:
+            if re.match(r'\d+\.\d+\.\d+\.\d+', addr) and (isinstance(mask, int) or re.match(r'\d+', mask)):
+                results.append((addr, str(mask)))
+            addr, mask = None, None
+    return results
