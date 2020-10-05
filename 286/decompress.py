b3d6b45600dd9e346752d484e91ed07377d57492
egandone <edone@sympatico.ca>
2020-10-05 09:17:44 -0400

    submission Bite 286 @ codechalleng.es

--- 286/decompress.py
+++ 286/decompress.py
@@ -2,14 +2,12 @@ from typing import Dict
 
 
 def decompress(string: str, table: Dict[str, str]) -> str:
-    result = string
-    while result and (set(table.keys()).intersection(set(result))):
-        new_result = ''
-        for c in result:
-            if c in table:
-                new_result += table[c]
-            else:
-                new_result += c
-        result = new_result
+    if (string):
+        key_set = set(table.keys())
+        # Keep iterating while the updated string still contains any
+        # characters that should be replaced.
+        while key_set.intersection(set(string)):
+            for k, v in table.items():
+                string = string.replace(k, v)
 
-    return result
+    return string
