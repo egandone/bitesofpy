d4d5eeb387a1600d2ecf1ca98f76f5b4f68cce7c
egandone <edone@sympatico.ca>
2020-10-06 15:33:36 -0400

    submission Bite 287 @ codechalleng.es

--- 287/sum_indices.py
+++ 287/sum_indices.py
@@ -2,4 +2,13 @@ from typing import List
 
 
 def sum_indices(items: List[str]) -> int:
-    ...
+    index_dict = dict()
+    s = 0
+    for i, v in enumerate(items):
+        if v in index_dict:
+            index_dict[v].append(i)
+            s += sum(index_dict[v])
+        else:
+            index_dict[v] = [i]
+            s += i
+    return s
