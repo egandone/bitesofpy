12fb9c47191096531a199c90a1b47b27b028d3dd
Egan Done <edone@sympatico.ca>
2020-10-09 20:46:47 -0400

    submission Bite 290 @ codechalleng.es

--- 290/class_rosters.py
+++ 290/class_rosters.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 
 def class_rosters(input_file):
@@ -6,4 +7,21 @@ def class_rosters(input_file):
         according to the Bite description.
         Return a list holding one item per student
         per class, correctly formatted.'''
-    pass
+    course_map = {}
+    with open(input_file) as f:
+        input_data = f.read()
+    for line in input_data.split('\n'):
+        if line.strip():
+            (id, first, last, *courses) = line.replace('"', '').split(',')
+            for course in courses:
+                if course:
+                    if course not in course_map:
+                        course_map[course] = []
+#                    if id not in course_map[course]:
+                    course_map[course].append(id)
+    result = []
+    for key, ids in course_map.items():
+        course = re.sub('\s-\s\S+$', '', key)
+        ids_str = ','.join(ids)
+        result.append(f'{course},2020,{ids_str}')
+    return result
