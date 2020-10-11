0d87cf35c0a4d2e169e56bf6f1956b0b4a41ce50
egandone <edone@sympatico.ca>
2020-10-11 16:11:25 -0400

    submission Bite 293 @ codechalleng.es

--- 293/n_digit_numbers.py
+++ 293/n_digit_numbers.py
@@ -2,5 +2,13 @@ from typing import List, TypeVar
 T = TypeVar('T', int, float)
 
 
+def n_digit_number(number, n):
+    factor = n - len(str(int(abs(number))))
+    return int(number * 10**factor)
+
+
 def n_digit_numbers(numbers: List[T], n: int) -> List[int]:
-    ...
+    if n < 1:
+        raise ValueError('n must be 1 or higher')
+    result = [n_digit_number(number, n) for number in numbers]
+    return result
